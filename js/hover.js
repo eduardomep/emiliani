c6c9232c6d0d363a103a412e57414d6daae855a1
eduardomep <eduardopeate@gmail.com>
2017-08-31 16:31:12 -0600

    Add files via upload

--- js/hover.js
+++ js/hover.js
@@ -6,28 +6,28 @@ $(document).ready(function() {
     });
 
     $('#btn-inicio').on("click", function(){
-       $('#carreras_general').css({"background-image":"url(img/fondo-carreras.JPG)"});
+       $('#carreras_general').css({"background-image":"url(img/fondo-carreras.jpg)"});
     });
     $('#btn-electronica').on("click", function(){
-       $('#carreras_general').css({"background-image":"url(img/electronica.JPG)"});
+       $('#carreras_general').css({"background-image":"url(img/electronica.jpg)"});
     });
     $('#btn-electricidad').on("click", function(){
-        $('#carreras_general').css({"background-image":"url(img/electricidad.JPG)"});
+        $('#carreras_general').css({"background-image":"url(img/electricidad.jpg)"});
     });
     $('#btn-mecanica').on("click", function(){
-        $('#carreras_general').css({"background-image":"url(img/mecanica.JPG)"});
+        $('#carreras_general').css({"background-image":"url(img/mecanica.jpg)"});
     });
     $('#btn-computacion').on("click", function(){
-       $('#carreras_general').css({"background-image":"url(img/computacion.JPG)"});
+       $('#carreras_general').css({"background-image":"url(img/computacion.jpg)"});
     });
     $('#btn-dibujo').on("click", function(){
-        $('#carreras_general').css({"background-image":"url(img/dibujo.JPG)"});
+        $('#carreras_general').css({"background-image":"url(img/dibujo.jpg)"});
     });
     $('#btn-ciencias').on("click", function(){
-        $('#carreras_general').css({"background-image":"url(img/ciencias.JPG)"});
+        $('#carreras_general').css({"background-image":"url(img/ciencias.jpg)"});
     });
      $('#btn-basicos').on("click", function(){
-        $('#carreras_general').css({"background-image":"url(img/basico.JPG)"});
+        $('#carreras_general').css({"background-image":"url(img/basico.jpg)"});
     });
 
 });
